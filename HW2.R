d7aafd9446c91a3c6b060954f43603d7f80e66bf
Jin Wen Lin <jinwen.lin@mail.utoronto.ca>
2024-09-11 16:23:52 -0400

    New update

--- HW2.R
+++ HW2.R
@@ -6,10 +6,15 @@
 #Put your code in this file. Make sure you assign the relevant values to the correct variable names, which are given below. 
 #Uncomment the variables as you assign your final values/functions/results to them.
 
-library(tidyverse)# This loads the packages necessary to run your plots. Do not delete or comment this out.
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(ggplot2)
+library(stringr)# This loads the packages necessary to run your plots. Do not delete or comment this out.
 
 ### Exercise 1
 
+library(tidyverse)
 spx_1Month <- read_csv("SPX-1Month.csv")
 
 spx_plot1<- ggplot(spx_1Month, mapping=aes(Date, `Close/Last`)) + 
@@ -21,17 +26,16 @@ spx_plot2<- spx_plot1 +
   theme(axis.text.x=element_text(angle=45,hjust=1)) 
 
 ### Exercise 2
-  
-bookGenres <- read_csv("BookGenres.csv")
-bookplot <- ggplot(
+
+bookGenres <- read_csv("BookGenres.csv")  
+bookplot<- ggplot(
   bookGenres,
-  mapping = aes(Category, `Price Starting With ($)`,
-    color = Season)) +
+  mapping = aes(Category, Price,
+                color = Season)) +
   geom_point(alpha = 0.4) + 
   labs(x = "Genre", y = "Price ($)", 
        title = "The Price of Fiction Books by Genre and Season") + theme_minimal() + 
-  theme(axis.text.x=element_text(angle=30,hjust=1)) 
-  
+  theme(axis.text.x=element_text(angle=30,hjust=1))
   
 ### Exercise 3
 
@@ -44,9 +48,9 @@ squirrelPlot<- ggplot(squirrel,
   
 ### Exercise 4
 
-big_mac <- read_csv("big_mac.csv")
+big_mac <- read_csv("big_mac.csv")  
 str(big_mac)
-big_mac$gdp<- as.numeric(big_mac$GDP.Per.Capita)
+gdp<- as.numeric(big_mac$GDP.Per.Capita)
 
 bigMac<- ggplot(big_mac, aes(gdp, dollar_price)) + 
   geom_point() + geom_smooth() + 
